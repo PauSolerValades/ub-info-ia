855d3d95d2f15a0a6773c9333867d8a62c73c143
PAU SOLER VALADES <psolerva7.alumnes@ub052229.mat.ub.edu>
2019-09-20 15:15:28 +0200

    dfs working

--- Practica1/search/search.py
+++ Practica1/search/search.py
@@ -90,7 +90,31 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    #axiò és un dfs que funciona, però falta gestionar les direccions
+    visitats = []
+    aVisitar = util.Stack()
+    aVisitar.push(problem.getStartState)
+    direccions=[]
+    
+    while not aVisitar.isEmpty():
+        nodeActual = aVisitar.pop()
+        coordNode = nodeActual[0]
+        dirNode = nodeActual[1]
+            
+        if coordNode in visitats: continue
+        visitats.append(coordNode)
+
+        direccions[coordNode]=dirNode
+        for i in problem.getSuccessors(coordNode):
+            aVisitar.push(i)
+
+        if problem.isGoalState(coordNode):
+            break
+            
+    for i in visitats:
+        direccions.append(i[1][1])
+        
+    return direccions
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
